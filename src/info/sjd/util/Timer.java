382ace6c5704545672e793796fa77778ba98e668
vtymashkov <Box8lister>
2019-04-19 15:40:43 +0300

    Timer has been refactored

--- src/info/sjd/util/Timer.java
+++ src/info/sjd/util/Timer.java
@@ -3,22 +3,13 @@ package info.sjd.util;
 public class Timer {
 
     public static String elapsed(long startTime, long endTime) {
-        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+        long duration = (endTime - startTime);  //divide by 1_000_000L to get milliseconds.
 
-        int nanoseconds = (int) (duration) % 60;
-        int milliseconds = (int) (duration / 1_000_000L) % 60;
-        int seconds = (int) (duration / 1_000_000_000L) % 60;
-        int minutes = (int) ((duration / (1_000_000_000L * 60)) % 60);
 
         double elapsedTimeInSecond = (double) duration / 1_000_000_000L;
 
-        return String.format("Elapsed - %f sec [" +
-                                "%d min, " +
-                                "%d sec, " +
-                                "%d millisec, " +
-                                "%d nanosec]",
-                elapsedTimeInSecond,
-                minutes, seconds, milliseconds, nanoseconds);
+        return String.format("Elapsed %f sec",
+                elapsedTimeInSecond);
 
     }
 }
